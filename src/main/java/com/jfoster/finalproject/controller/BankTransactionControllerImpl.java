939456b5ac9a0be453038bbdee6e8b715c46f35a
jordanofoster <46407321+jordanofoster@users.noreply.github.com>
2024-11-10 13:44:02 +0000

    Moved TimestampEditor into anonymous class within BankTransactionControllerImpl, since that's the only place we use it.

--- src/main/java/com/jfoster/finalproject/controller/BankTransactionControllerImpl.java
+++ src/main/java/com/jfoster/finalproject/controller/BankTransactionControllerImpl.java
@@ -12,7 +12,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.beans.PropertyEditorSupport;
 import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 @RestController
@@ -25,7 +28,18 @@ public class BankTransactionControllerImpl implements BankTransactionController 
     @InitBinder
     @Override
     public void initBinder(WebDataBinder binder) {
-        binder.registerCustomEditor(Timestamp.class, new TimestampEditor());
+        binder.registerCustomEditor(Timestamp.class, new PropertyEditorSupport() {
+            private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException {
+                try {
+                    setValue(new Timestamp(dateFormat.parse(text).getTime()));
+                } catch (ParseException e) {
+                    throw new IllegalArgumentException(e);
+                }
+            }
+        });
     }
 
     public ResponseEntity<BankTransactionImpl> createTransaction(@RequestBody BankTransactionImpl bankTransactionObj) {
